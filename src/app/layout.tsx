cacfa3c216ce0fe548ba9d2c41e6e05dfa51ac6b
utkarsh <utkarshsingh132003@gmail.com>
2025-05-03 18:40:11 +0530

    google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,8 @@ import "./globals.css";
 import Navbar from "@/components/custom/Navbar";
 import Footer from "@/components/custom/Footer";
 import { Toaster } from "@/components/ui/sonner";
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 const poppinsFonts = Poppins({
   weight: "500",
@@ -23,7 +22,8 @@ export const metadata: Metadata = {
   description:
     "Access thousands of final year exam papers from top universities across various subjects. Search, download, and prepare confidently with ExamPaperHub – your go-to resource for academic success.",
   openGraph: {
-    images: "http://res.cloudinary.com/dakddv1pm/image/upload/v1746276103/posts/zan4ctonv0n0efq0ppad.png",
+    images:
+      "http://res.cloudinary.com/dakddv1pm/image/upload/v1746276103/posts/zan4ctonv0n0efq0ppad.png",
   },
 };
 
@@ -33,19 +33,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider  appearance={{
-      layout: {
-        unsafe_disableDevelopmentModeWarnings: true,
-      },
-    }}>
-    <html lang="en">
-      <body className={`${poppinsFonts.className} ${geistMono.variable} antialiased`}>
-        <Toaster/>
-        <Navbar/>
-        {children}
-        <Footer/>
-      </body>
-    </html>
-  </ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
+      <html lang="en">
+        <body
+          className={`${poppinsFonts.className} ${geistMono.variable} antialiased`}
+        >
+            <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RL3V9F5XCF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RL3V9F5XCF');
+          `}
+        </Script>
+          <Toaster />
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
